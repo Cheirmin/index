d3c2edb66cbdabd161a57e7e1d60c676712c0a6f
Cheirmin <1147905069@qq.com>
2018-11-21 19:08:56 +0800

    index.php

---
+++ index.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html lang="zh-CN">
+	<head>
+		<meta charset="utf-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<title>Cheirmin</title>
+		  <!-- Bootstrap CSS 文件 -->
+		<link rel="stylesheet" href="/index/static/bootstrap/css/bootstrap.min.css">
+	</head>
+	<body>
+
+	<!-- 头部 -->
+		<div class="jumbotron">
+			<div class="container">
+				<h2>Welcome To Cheirmin's Website!</h2>
+				<p></p>
+				<p>--If you take my wine wild life.</p>
+			</div>
+		</div>
+
+	<!-- 中间内容区局 -->
+	<div class="container">
+		<div class="row">
+
+			<!-- 左侧菜单区域   -->
+			<div class="col-md-3">
+					<div class="list-group">
+						<a href="\Just_For_Fun\Love-master\index.html" class="list-group-item">Love Story</a>
+					</div>
+			</div>
+			
+			<!-- 右侧内容区域 -->
+			<div class="col-md-3">
+				<img src='\index\photo\0.png'>
+			</div>
+		   
+		</div>
+	</div>
+	
+	 <div class="jumbotron" style="margin:0;">
+        <div class="container">
+            <span>  @2018 Cheirmin </span>
+        </div>
+    </div>
+
+	<!-- jQuery 文件 -->
+	<script language='JavaScript' src="/index/static/jquery/jquery.min.js" ></script>
+	<!-- Bootstrap JavaScript 文件 -->
+	<script language='JavaScript' src='/index/static/bootstrap/js/bootstrap.min.js'></script>
+	
+	</body>
+	
+</html>
